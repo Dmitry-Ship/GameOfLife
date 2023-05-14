3092a8cccfe0f107e115145d0892d45b1aff4073
Dmitry Shipunov <vooxil@gmail.com>
2023-05-15 01:11:28 +0300

    move methods to world

--- gameOfLife/world.go
+++ gameOfLife/world.go
@@ -33,20 +33,41 @@ func (w *World) GetCells() [][]Cell {
 	return w.cells
 }
 
+func (w *World) GetCellNextState(c *Cell, y, x int) Cell {
+	neighbors := w.getAliveNeighbors(y, x)
+	// Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+	if !c.IsAlive && neighbors == 3 {
+		return Cell{IsAlive: true}
+	}
+
+	// Any live cell with more than three live neighbours dies, as if by overpopulation.
+	if c.IsAlive && neighbors > 3 {
+		return Cell{IsAlive: false}
+	}
+
+	// Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+	if c.IsAlive && neighbors < 2 {
+		return Cell{IsAlive: false}
+	}
+
+	// Any live cell with two or three live neighbours lives on to the next generation.
+	if c.IsAlive && (neighbors == 2 || neighbors == 3) {
+		return Cell{IsAlive: true}
+	}
+
+	return Cell{IsAlive: c.IsAlive}
+}
+
 func (w *World) NextGeneration() {
-	newCells := make([][]Cell, w.height, w.height)
+	newCells := make([][]Cell, w.height)
 	for i := range newCells {
-		newCells[i] = make([]Cell, w.width, w.width)
+		newCells[i] = make([]Cell, w.width)
 	}
 
 	for y := range w.cells {
 		for x, oldCell := range w.cells[y] {
-			oldNeighbors := w.getAliveNeighbors(y, x)
 
-			newCell := Cell{IsAlive: oldCell.IsAlive}
-			newCell.nextState(oldNeighbors)
-
-			newCells[y][x] = newCell
+			newCells[y][x] = w.GetCellNextState(&oldCell, y, x)
 		}
 	}
 
@@ -57,14 +78,18 @@ func (w *World) getAliveNeighbors(cellY, cellX int) int {
 	aliveNeighbors := 0
 
 	for y := -1; y <= 1; y++ {
+		// out of bounds
+		if cellY+y < 0 || cellY+y >= w.height {
+			continue
+		}
 		for x := -1; x <= 1; x++ {
-			// self
-			if x == 0 && y == 0 {
+			// out of bounds
+			if cellX+x < 0 || cellX+x >= w.width {
 				continue
 			}
 
-			// out of bounds
-			if cellY+y < 0 || cellY+y >= w.height || cellX+x < 0 || cellX+x >= w.width {
+			// self
+			if x == 0 && y == 0 {
 				continue
 			}
 
